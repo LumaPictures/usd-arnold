51a26b86a1d6ccb6f37d7a0b6d03b69917f6b1f2
Pal Mezei <palm@lumapictures.com>
2017-06-16 12:14:03 +1000

    Adding files not covered by schema gen.

---
+++ lib/module.cpp
@@ -0,0 +1,12 @@
+#include "pxr/base/tf/pyModule.h"
+
+PXR_NAMESPACE_USING_DIRECTIVE
+
+TF_WRAP_MODULE
+{
+    TF_WRAP(UsdAiMaterialAPI);
+    TF_WRAP(UsdAiNodeAPI);
+    TF_WRAP(UsdAiProcedural);
+    TF_WRAP(UsdAiShader);
+    TF_WRAP(UsdAiVolume);
+}
