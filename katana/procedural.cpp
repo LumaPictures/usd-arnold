109f1041f3e710515896740ba12241cbcef31877
Pal Mezei <palm@lumapictures.com>
2017-06-28 16:33:55 +1000

    Registering and loading ops.

---
+++ katana/procedural.cpp
@@ -0,0 +1,34 @@
+//.#include "pxrUsdInShipped/declareCoreOps.h"
+
+
+#include <usdKatana/attrMap.h>
+#include <usdKatana/usdInPluginRegistry.h>
+
+#include <pxr/usd/usdAi/aiProcedural.h>
+#include <pxr/usd/usdAi/aiVolume.h>
+
+#include "readProcedural.h"
+
+PXR_NAMESPACE_USING_DIRECTIVE
+
+PXRUSDKATANA_USDIN_PLUGIN_DECLARE(AiProceduralOp)
+
+PXRUSDKATANA_USDIN_PLUGIN_DEFINE(AiProceduralOp, privateData, interface)
+{
+    PxrUsdKatanaAttrMap attrs;
+
+    PxrUsdKatanaReadAiProcedural(
+        UsdAiProcedural(privateData.GetUsdPrim()), privateData, attrs);
+
+    attrs.toInterface(interface);
+}
+
+DEFINE_GEOLIBOP_PLUGIN(AiProceduralOp)
+
+void registerPlugins()
+{
+    REGISTER_PLUGIN(AiProceduralOp, "AiProceduralOp", 0, 1);
+
+    PxrUsdKatanaUsdInPluginRegistry::RegisterUsdType<UsdAiProcedural>("PxrUsdInCore_AiProceduralOp");
+    PxrUsdKatanaUsdInPluginRegistry::RegisterUsdType<UsdAiVolume>("PxrUsdInCore_AiProceduralOp");
+}
