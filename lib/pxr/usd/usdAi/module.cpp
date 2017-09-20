cf12d1caa0319d6ca52725140e98ea1f23f6b1d1
Chad Dombrova <chadrik@gmail.com>
2017-09-20 15:41:18 -0700

    Add AiAOV and AiShapeAPI to python bindings

--- lib/pxr/usd/usdAi/module.cpp
+++ lib/pxr/usd/usdAi/module.cpp
@@ -5,10 +5,12 @@ PXR_NAMESPACE_USING_DIRECTIVE
 
 TF_WRAP_MODULE
 {
+    TF_WRAP(UsdAiAOV);
     TF_WRAP(UsdAiMaterialAPI);
     TF_WRAP(UsdAiNodeAPI);
     TF_WRAP(UsdAiProcedural);
     TF_WRAP(UsdAiShader);
     TF_WRAP(UsdAiShaderExport);
+    TF_WRAP(UsdAiShapeAPI);
     TF_WRAP(UsdAiVolume);
 }
