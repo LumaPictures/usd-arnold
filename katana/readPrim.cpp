72ae666d86a1234266118c110def8c3b8deea839
Pal Mezei <sirpalee@gmail.com>
2017-10-03 10:45:04 +1100

    Running the read arnold statements on every location if it makes sense.

--- katana/readPrim.cpp
+++ katana/readPrim.cpp
@@ -1,5 +1,10 @@
 #include "readPrim.h"
 
+#include <pxr/usd/usdAi/aiShapeAPI.h>
+#include <usdKatana/attrMap.h>
+
+#include "arnoldHelpers.h"
+
 PXR_NAMESPACE_OPEN_SCOPE
 
 void readPrimLocation(
@@ -8,6 +13,27 @@ void readPrimLocation(
     PxrUsdKatanaUsdInPrivateData* privateData) {
     // privateData can be a nullptr!
     if (privateData == nullptr) { return; }
+    const auto prim = privateData->GetUsdPrim();
+    if (!prim.IsValid()) { return; }
+
+    UsdAiShapeAPI shapeAPI(prim);
+    if (shapeAPI) {
+        FnKat::GroupAttribute arnoldStatements = GetArnoldStatementsGroup(prim);
+        static constexpr auto statementsName = "arnoldStatements";
+        // This code is taken from PxrUsdKatanaAttrMap::toInterface
+        // so we don't have to build some dynamic lists just for this.
+        auto existingAttr = interface.getOutputAttr(statementsName);
+        if (existingAttr.isValid()) {
+            interface.setAttr(statementsName,
+                FnAttribute::GroupBuilder()
+                    .update(existingAttr)
+                    .deepUpdate(arnoldStatements)
+                    .build());
+        } else {
+            // if it's not a GroupAttr we need to call this
+            interface.setAttr(statementsName, arnoldStatements);
+        }
+    }
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE
