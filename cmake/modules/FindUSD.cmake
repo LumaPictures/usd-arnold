e038faabc52cafcb80a2feb29d8380f227ab2952
Pal Mezei <palm@lumapictures.com>
2018-02-08 09:49:55 +1100

    Adding more houdini plugin related cmake code.

--- cmake/modules/FindUSD.cmake
+++ cmake/modules/FindUSD.cmake
@@ -19,6 +19,14 @@ find_file(USD_GENSCHEMA
 
 # USD Maya components
 
+find_path(USD_MAYA_INCLUDE_DIR usdMaya/api.h
+          PATHS ${USD_ROOT}/third_party/maya/include
+          DOC "USD Maya Include directory")
+
+find_path(USD_MAYA_LIBRARY_DIR libusdMaya.so
+          PATHS ${USD_ROOT}/third_party/maya/lib
+          DOC "USD Maya Library directory")
+
 # USD Katana components
 
 find_path(USD_KATANA_INCLUDE_DIR usdKatana/api.h
@@ -28,6 +36,16 @@ find_path(USD_KATANA_INCLUDE_DIR usdKatana/api.h
 find_path(USD_KATANA_LIBRARY_DIR libusdKatana.so
           PATHS ${USD_ROOT}/third_party/katana/lib
           DOC "USD Katana Library directory")
+
+# USD Houdini components
+
+find_path(USD_HOUDINI_INCLUDE_DIR gusd/api.h
+          PATHS ${USD_ROOT}/third_party/houdini/include
+          DOC "USD Houdini Include directory")
+
+find_path(USD_HOUDINI_LIBRARY_DIR libgusd.so
+          PATHS ${USD_ROOT}/third_party/houdini/lib
+          DOC "USD Houdini Library directory")
 
 if(USD_INCLUDE_DIR AND EXISTS "${USD_INCLUDE_DIR}/pxr/pxr.h")
     foreach(_usd_comp MAJOR MINOR PATCH)
