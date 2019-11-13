9678a1b456251731be2ba1ba3d33ea9342dfbb7b
Paul Molodowitch <paulm@lumapictures.com>
2019-11-13 14:46:46 -0800

    FindUSD: standardize indentation

--- cmake/modules/FindUSD.cmake
+++ cmake/modules/FindUSD.cmake
@@ -20,66 +20,80 @@ else ()
 endif ()
 
 find_path(USD_INCLUDE_DIR pxr/pxr.h
-    PATHS ${USD_ROOT}/include
-          $ENV{USD_ROOT}/include
+    PATHS
+        ${USD_ROOT}/include
+        $ENV{USD_ROOT}/include
     DOC "USD Include directory")
 
 # We need to find either usd or usd_ms, with taking the prefix into account.
 find_path(USD_LIBRARY_DIR
-    NAMES ${USD_LIB_PREFIX}usd${USD_LIB_EXTENSION} ${USD_LIB_PREFIX}usd_ms${USD_LIB_EXTENSION}
-    PATHS ${USD_ROOT}/lib
-          $ENV{USD_ROOT}/lib
+    NAMES
+        ${USD_LIB_PREFIX}usd${USD_LIB_EXTENSION}
+        ${USD_LIB_PREFIX}usd_ms${USD_LIB_EXTENSION}
+    PATHS
+        ${USD_ROOT}/lib
+        $ENV{USD_ROOT}/lib
     DOC "USD Libraries directory")
 
 # USD Maya components
 
 find_path(USD_MAYA_INCLUDE_DIR usdMaya/api.h
-    PATHS ${USD_ROOT}/third_party/maya/include
-          $ENV{USD_ROOT}/third_party/maya/include
-          ${USD_MAYA_ROOT}/third_party/maya/include
-          $ENV{USD_MAYA_ROOT}/third_party/maya/include
+    PATHS
+        ${USD_ROOT}/third_party/maya/include
+        $ENV{USD_ROOT}/third_party/maya/include
+        ${USD_MAYA_ROOT}/third_party/maya/include
+        $ENV{USD_MAYA_ROOT}/third_party/maya/include
     DOC "USD Maya Include directory")
 
 find_path(USD_MAYA_LIBRARY_DIR
-    NAMES ${USD_LIB_PREFIX}usdMaya${USD_LIB_EXTENSION}
-    PATHS ${USD_ROOT}/third_party/maya/lib
-          $ENV{USD_ROOT}/third_party/maya/lib
-          ${USD_MAYA_ROOT}/third_party/maya/lib
-          $ENV{USD_MAYA_ROOT}/third_party/maya/lib
+    NAMES
+        ${USD_LIB_PREFIX}usdMaya${USD_LIB_EXTENSION}
+    PATHS
+        ${USD_ROOT}/third_party/maya/lib
+        $ENV{USD_ROOT}/third_party/maya/lib
+        ${USD_MAYA_ROOT}/third_party/maya/lib
+        $ENV{USD_MAYA_ROOT}/third_party/maya/lib
     DOC "USD Maya Library directory")
 
 # USD Katana components
 
 find_path(USD_KATANA_INCLUDE_DIR usdKatana/api.h
-    PATHS ${USD_ROOT}/third_party/katana/include
-          $ENV{USD_ROOT}/third_party/katana/include
-          ${USD_KATANA_ROOT}/third_party/katana/include
-          $ENV{USD_KATANA_ROOT}/third_party/katana/include
+    PATHS
+        ${USD_ROOT}/third_party/katana/include
+        $ENV{USD_ROOT}/third_party/katana/include
+        ${USD_KATANA_ROOT}/third_party/katana/include
+        $ENV{USD_KATANA_ROOT}/third_party/katana/include
     DOC "USD Katana Include directory")
 
 find_path(USD_KATANA_LIBRARY_DIR
-          NAMES ${USD_LIB_PREFIX}usdKatana${USD_LIB_EXTENSION}
-          PATHS ${USD_ROOT}/third_party/katana/lib
-                $ENV{USD_ROOT}/third_party/katana/lib
-                ${USD_KATANA_ROOT}/third_party/katana/lib
-                $ENV{USD_KATANA_ROOT}/third_party/katana/lib
-          DOC "USD Katana Library directory")
+    NAMES
+        ${USD_LIB_PREFIX}usdKatana${USD_LIB_EXTENSION}
+    PATHS
+        ${USD_ROOT}/third_party/katana/lib
+        $ENV{USD_ROOT}/third_party/katana/lib
+        ${USD_KATANA_ROOT}/third_party/katana/lib
+        $ENV{USD_KATANA_ROOT}/third_party/katana/lib
+    DOC "USD Katana Library directory")
 
 # USD Houdini components
 
 find_path(USD_HOUDINI_INCLUDE_DIR gusd/api.h
-          PATHS ${USD_ROOT}/third_party/houdini/include
-                $ENV{USD_ROOT}/third_party/houdini/include
-                ${USD_HOUDINI_ROOT}/third_party/houdini/include
-                $ENV{USD_HOUDINI_ROOT}/third_party/houdini/include
-          DOC "USD Houdini Include directory")
+    PATHS
+        ${USD_ROOT}/third_party/houdini/include
+        $ENV{USD_ROOT}/third_party/houdini/include
+        ${USD_HOUDINI_ROOT}/third_party/houdini/include
+        $ENV{USD_HOUDINI_ROOT}/third_party/houdini/include
+    DOC "USD Houdini Include directory")
 
-find_path(USD_HOUDINI_LIBRARY_DIR ${USD_LIB_PREFIX}gusd${USD_LIB_EXTENSION}
-          PATHS ${USD_ROOT}/third_party/houdini/lib
-                $ENV{USD_ROOT}/third_party/houdini/lib
-                ${USD_HOUDINI_ROOT}/third_party/houdini/lib
-                $ENV{USD_HOUDINI_ROOT}/third_party/houdini/lib
-          DOC "USD Houdini Library directory")
+find_path(USD_HOUDINI_LIBRARY_DIR
+    NAMES
+        ${USD_LIB_PREFIX}gusd${USD_LIB_EXTENSION}
+    PATHS 
+        ${USD_ROOT}/third_party/houdini/lib
+        $ENV{USD_ROOT}/third_party/houdini/lib
+        ${USD_HOUDINI_ROOT}/third_party/houdini/lib
+        $ENV{USD_HOUDINI_ROOT}/third_party/houdini/lib
+    DOC "USD Houdini Library directory")
 
 if(USD_INCLUDE_DIR AND EXISTS "${USD_INCLUDE_DIR}/pxr/pxr.h")
     foreach(_usd_comp MAJOR MINOR PATCH)
